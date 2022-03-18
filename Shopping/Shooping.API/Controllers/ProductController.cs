fac4e131097e8704a60ca54af7491207d9d88bd4
Francisco <franciscogaletti@yahoo.com>
2022-03-17 23:48:27 -0500

    Added Shooping.API

---
+++ Shopping/Shooping.API/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shooping.API.Model;
+using Shopping.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shooping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+            //var rng = new Random();
+
+            //return Enumerable.Range(1, 5).Select(index =>
+            // new Product
+            // {
+            //     Name = "aaa"
+            // }
+            //).ToArray();
+        }
+
+    }
+}
--- Shopping/Shopping.Client/Startup.cs
+++ Shopping/Shopping.Client/Startup.cs
@@ -22,7 +22,12 @@ namespace Shopping.Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("ShoppingApiClient", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/");
+            });
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
